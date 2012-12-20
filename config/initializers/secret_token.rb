2c2f0ce94f9a988329501db68b8ca729fb84169f
Andrew Cranston <andrewc@aurorafeint.com>
2012-12-19 21:16:21 -0800

    initial files for ruby3 test project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinosaur::Application.config.secret_token = '8e181fb667ddbb0ae7ee04b98e8e32359eb43a8aa601f8d2db624d63d85ba03193285638d88d95aa93843c0548ffd0570b01bd77d62de1179de9c265bd7727c7'
